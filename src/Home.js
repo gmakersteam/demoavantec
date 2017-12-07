797db96e2cabee0c0603d1091987a00824e7076c
gmakersteam <gmakers.team@gmail.com>
2017-12-06 23:37:14 -0500

    Home

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import './css/app.css';
+
+export const Home = () =>{
+    return(
+        <div>Hola Home</div>
+    )
+} 
