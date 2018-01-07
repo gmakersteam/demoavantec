3982994a58f7980aa6bbc3dd84868b08b719b987
GLADYSYOVANAMAMANICAMATICONA <gladysyovanamamanicamaticona@gmail.com>
2018-01-07 18:06:14 -0500

    gladys

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import './css/app.css';
+import Contactame from './contacto'
 
 export const Home = () =>{
     return(
-        <div>Hola Home</div>
+        <div>
+        {/* <div>Hola Home</div> */}
+        <Contactame/>
+        </div>
     )
 } 
