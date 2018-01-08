3f81072e38ace860a52ebe68fe96d14d523ca9e7
MelanieOC <melanie.ocharan@gmail.com>
2018-01-08 08:41:52 -0500

    productos terminado

--- src/Productos.js
+++ src/Productos.js
@@ -2,52 +2,90 @@ import React from 'react';
 // import Utils from './Utils.js';
 import { NavLink } from 'react-router-dom'
 // import Header from './Header';
-//import './Productos.css';
-import imagen from './probbb.png';
+import './Productos.css';
+import img_prueba from './probbb.png';
 
+const Producto = ({ imagen, nombre, precio, oferta }) => {
+    return (
+        <div className="col-md-3 col-sm-3 col-xs-3 producto">
+            <NavLink to={'/Detalles'}>
+                <img className="img-responsive" src={imagen} alt="" />
+            </NavLink>
+            <h3>
+                <NavLink to={'/Detalles'}>{nombre}</NavLink>
+            </h3>
+            <div className='precio'>
+                <p className={oferta ? 'old' : 'precio'}>S/. {precio}</p>
+                {
+                    oferta && <p className='precio oferta'>S/. {oferta}</p>
+                }
+            </div>
+        </div>
+    )
+}
 const Productos = () => {
     return (
-        <div>
+        <div id='Productos'>
             <h1>Nuestros productos</h1>
             <section id="catalogo">
 
                 <div className="container">
                     <div className="row">
                         <div id="myCarousel" className="carousel slide col-md-10 col-sm-12 col-xs-12">
+                            <ol className="carousel-indicators">
+                                <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+                                <li data-target="#myCarousel" data-slide-to="1"></li>
+                            </ol>
                             <div className="carousel-inner">
                                 <div className="item active">
                                     <div className="row-fluid">
-                                        <div className="col-md-3 col-sm-3 col-xs-3"><a href="nintendo.html"><img className="img-responsive" src={imagen} alt="" /></a></div>
-                                        <div className="col-md-3 col-sm-3 col-xs-3"><a href="nintendo.html"><img className="img-responsive" src={imagen} alt="" /></a></div>
-                                        <div className="col-md-3 col-sm-3 col-xs-3"><a href="nintendo.html"><img className="img-responsive" src={imagen} alt="" /></a></div>
-                                        <div className="col-md-3 col-sm-3 col-xs-3"><a href="nintendo.html"><img className="img-responsive" src={imagen} alt="" /></a></div>
+                                        <Producto imagen={img_prueba} precio={999.99} oferta={899.99} nombre={'Producto'} />
+                                        <Producto imagen={img_prueba} precio={999.99} nombre={'Producto'} />
+                                        <Producto imagen={img_prueba} precio={999.99} oferta={899.99} nombre={'Producto'} />
+                                        <Producto imagen={img_prueba} precio={999.99} oferta={899.99} nombre={'Producto'} />
                                     </div>
                                 </div>
                                 <div className="item">
                                     <div className="row-fluid">
-                                        <div className="col-md-3 col-sm-3 col-xs-3"><a href="nintendo.html"><img className="img-responsive" src={imagen} alt="" /></a></div>
-                                        <div className="col-md-3 col-sm-3 col-xs-3"><a href="nintendo.html"><img className="img-responsive" src={imagen} alt="" /></a></div>
-                                        <div className="col-md-3 col-sm-3 col-xs-3"><a href="nintendo.html"><img className="img-responsive" src={imagen} alt="" /></a></div>
-                                        <div className="col-md-3 col-sm-3 col-xs-3"><a href="nintendo.html"><img className="img-responsive" src={imagen} alt="" /></a></div>
+                                        <Producto imagen={img_prueba} precio={999.99} nombre={'Producto'} oferta={899.99} />
+                                        <Producto imagen={img_prueba} precio={999.99} nombre={'Producto'} />
+                                        <Producto imagen={img_prueba} precio={999.99} nombre={'Producto'} />
+                                        <Producto imagen={img_prueba} precio={999.99} nombre={'Producto'} />
                                     </div>
                                 </div>
                             </div>
-
-                            <a className="left carousel-control" href="#myCarousel" data-slide="prev">
-                                <span className="glyphicon glyphicon-chevron-left"></span>
-                                <span className="sr-only">Previous</span>
-                            </a>
-                            <a className="right carousel-control" href="#myCarousel" data-slide="next">
-                                <span className="glyphicon glyphicon-chevron-right"></span>
-                                <span className="sr-only">Next</span>
-                            </a>
                         </div>
-                        <div className="col-md-1 centro"><a href="nintendo.html">Mas resultados</a></div>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div id="myCarousel2" className="carousel slide col-md-10 col-sm-12 col-xs-12">
+                            <ol className="carousel-indicators">
+                                <li data-target="#myCarousel2" data-slide-to="0" className="active"></li>
+                                <li data-target="#myCarousel2" data-slide-to="1"></li>
+                            </ol>
+                            <div className="carousel-inner">
+                                <div className="item active">
+                                    <div className="row-fluid">
+                                        <Producto imagen={img_prueba} precio={999.99} oferta={899.99} nombre={'Producto'} />
+                                        <Producto imagen={img_prueba} precio={999.99} nombre={'Producto'} />
+                                        <Producto imagen={img_prueba} precio={999.99} oferta={899.99} nombre={'Producto'} />
+                                        <Producto imagen={img_prueba} precio={999.99} oferta={899.99} nombre={'Producto'} />
+                                    </div>
+                                </div>
+                                <div className="item">
+                                    <div className="row-fluid">
+                                        <Producto imagen={img_prueba} precio={999.99} nombre={'Producto'} oferta={899.99} />
+                                        <Producto imagen={img_prueba} precio={999.99} nombre={'Producto'} />
+                                        <Producto imagen={img_prueba} precio={999.99} nombre={'Producto'} />
+                                        <Producto imagen={img_prueba} precio={999.99} nombre={'Producto'} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
-            <hr />
-            <NavLink to={'/Detalles'}>Mas detalles</NavLink>
         </div>
     )
 }
