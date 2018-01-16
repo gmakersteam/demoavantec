39a12c6ee928c890c80d177b1fbcb78255e124ec
gmakersteam <gmakers.team@gmail.com>
2018-01-16 12:24:35 -0500

    contact

--- src/contacto.js
+++ src/contacto.js
@@ -11,7 +11,23 @@ import registerServiceWorker from './registerServiceWorker';
 const Contactme = () => {
     return (
         <section id='contact'>
-            <Row  className='text-center contacto'>
+            <Row className='text-center contacto'>
+                <Col xs={12} md={6} >
+                    <p>Medios de Pago</p>
+                    <ul className='medios_pago' >
+                        <li><img src='./img/contact/Paypal.svg' /></li>
+                        <li><img src='./img/contact/Visa.svg' /></li>
+                    </ul>
+                </Col>
+                <Col xs={12} md={6}>
+                    <p>Medios de Pago</p>
+                    <ul className='medios_pago'>
+                        <li><img src='./img/contact/mercadoLibre.png' /></li>
+                        <li><a target='_blank' href='http://www.olx.com.pe/'><img src='./img/contact/olx.png' /></a></li>
+                    </ul>
+                </Col>
+            </Row>
+            <Row  className='row-linea text-center contacto'>
                 <Col  xs={12} md={5} className='columns'>
                 <p >Visítanos</p>
                     <ul className='direction-pago'>
@@ -24,20 +40,12 @@ const Contactme = () => {
                         {/* <iframe className="g-mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827.4644814232556!2d-71.53253547631482!3d-16.40122009641886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91424a55c8ac1627%3A0x541109278d97b8a4!2sNova+Center%2C+Octavio+Mu%C3%B1oz+Najar%2C+Arequipa+Metropolitana!5e0!3m2!1ses!2spe!4v1512756110197" width="100%" height="225" frameBorder="0" allowFullScreen></iframe> */}
                     </ul>
                 </Col>
-                <Col xs={12} md={3} className='columns'>
-                    <p>Contactanos</p>
-                    <ul className='contact-number'>
-                        <li ><i className="fa fa-phone-square " aria-hidden="true"></i> 977371550 - 949510000</li>
-                        <li ><i className="fa fa-envelope " aria-hidden="true"></i> ventas@avantecperu.com</li>
-                    </ul>
+                <Col xs={12} md={6} className='columns'>
+                    <iframe width="600" height="300" frameBorder="0" style={{border:"0"}} allowFullScreen src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d521.7333380728154!2d-71.53004819124676!3d-16.401822124224466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91424a55c4e099d5%3A0xb64798c1831690dd!2sCompuplaza!5e0!3m2!1ses!2spe!4v1516121602949"></iframe>
                 </Col>
-                <Col xs={12} md={3} className='columns'>
-                    <p>Medios de Pago</p>
-                    <ul className='medios_pago'>
-                        <li><img src='./img/pago/Paypal.svg' /></li>
-                        <li><img src='./img/pago/Visa.svg' /></li>
-                    </ul>
-                </Col>
+                {/* <Col xs={12} md={3} className='columns'>
+                    
+                </Col> */}
                 <Col xs={12} md={1}>
                     <p>Siguenos</p>
                 </Col>
