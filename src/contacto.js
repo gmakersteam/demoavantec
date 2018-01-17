6328bf1d154e86a561cf2a98bfbed8e711610887
gmakersteam <gmakers.team@gmail.com>
2018-01-17 10:50:34 -0500

    map embed

--- src/contacto.js
+++ src/contacto.js
@@ -47,7 +47,9 @@ const Contactme = () => {
                     </ul>
                 </Col>
                 <Col xs={12} md={6} className='columns'>
-                    <iframe width="600" height="300" frameBorder="0" style={{border:"0"}} allowFullScreen src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d521.7333380728154!2d-71.53004819124676!3d-16.401822124224466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91424a55c4e099d5%3A0xb64798c1831690dd!2sCompuplaza!5e0!3m2!1ses!2spe!4v1516121602949"></iframe>
+                    <div className='map-responsive'>
+                        <iframe width="600" height="300" frameBorder="0" style={{border:"0"}} allowFullScreen src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d521.7333380728154!2d-71.53004819124676!3d-16.401822124224466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91424a55c4e099d5%3A0xb64798c1831690dd!2sCompuplaza!5e0!3m2!1ses!2spe!4v1516121602949"></iframe>
+                    </div>
                 </Col>
                 {/* <Col xs={12} md={3} className='columns'>
                     
