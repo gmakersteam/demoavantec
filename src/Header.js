0f724ef05ea04b581291a66b683889914bc0d6d0
gmakersteam <gmakers.team@gmail.com>
2018-01-17 01:42:36 -0500

    navbar

--- src/Header.js
+++ src/Header.js
@@ -75,23 +75,7 @@ const Menu = () => {
 const Categorias = () => {
 		return (
 				<Navbar fixedBottom collapseOnSelect>
-						
 						<Navbar.Collapse>
-								{/* <Nav>
-				<NavItem eventKey={1} href="#">
-					Gadgets
-				</NavItem>
-				<NavItem eventKey={2} href="#">
-					Computadoras
-				</NavItem>
-				<NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-					<MenuItem eventKey={3.1}>Action</MenuItem>
-					<MenuItem eventKey={3.2}>Another action</MenuItem>
-					<MenuItem eventKey={3.3}>Something else here</MenuItem>
-					<MenuItem divider />
-					<MenuItem eventKey={3.3}>Separated link</MenuItem>
-				</NavDropdown>
-			</Nav> */}
 								<Nav pullRight>
 										<NavItem eventKey={1} href="#">
 												Gadgets
@@ -100,15 +84,8 @@ const Categorias = () => {
 												Computadoras
 										</NavItem>
 										<NavItem eventKey={3} href="#">
-											Equipos Biometricos
+											Equipos_Biometricos
 										</NavItem>
-										{/* <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-					<MenuItem eventKey={3.1}>Action</MenuItem>
-					<MenuItem eventKey={3.2}>Another action</MenuItem>
-					<MenuItem eventKey={3.3}>Something else here</MenuItem>
-					<MenuItem divider />
-					<MenuItem eventKey={3.3}>Separated link</MenuItem>
-				</NavDropdown> */}
 										<NavItem eventKey={4} href="#">
 												Laptops
 										</NavItem>
@@ -120,9 +97,6 @@ const Categorias = () => {
 										</NavItem>
 										<NavItem eventKey={7} href="#">
 												Drones
-										</NavItem>
-										<NavItem eventKey={8} href="#">
-												Accesorios
 										</NavItem>
 								</Nav>
 						</Navbar.Collapse>
--- src/contacto.js
+++ src/contacto.js
@@ -29,6 +29,7 @@ const Contactme = () => {
                 <Col xs={12} md={4}>
                     <div style={{textAlign: "center" }}>
                         <script type="text/javascript" src="//rf.revolvermaps.com/0/0/6.js?i=5s8wq4wsqcz&amp;m=7&amp;c=e63100&amp;cr1=ffffff&amp;f=arial&amp;l=0&amp;bv=90&amp;lx=-420&amp;ly=420&amp;hi=20&amp;he=7&amp;hc=a8ddff&amp;rs=80" async="async"></script>
+                        {/* <iframe src="//rf.revolvermaps.com/0/0/6.js?i=5s8wq4wsqcz&amp;m=7&amp;c=e63100&amp;cr1=ffffff&amp;f=arial&amp;l=0&amp;bv=90&amp;lx=-420&amp;ly=420&amp;hi=20&amp;he=7&amp;hc=a8ddff&amp;rs=80" async="async"></iframe> */}
                     </div>
                 </Col>
             </Row>
