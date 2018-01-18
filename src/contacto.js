527542eb37a62d9f21f1ed28be9f6cb4a218230d
gmakersteam <gmakers.team@gmail.com>
2018-01-17 22:39:57 -0500

    contact

--- src/contacto.js
+++ src/contacto.js
@@ -27,7 +27,7 @@ const Contactme = () => {
                     </ul>
                 </Col>
                 <Col xs={12} md={4}>    
-                            <div id="revolvermap" align="center"  className="wcustomhtml">
+                            <div id="revolvermap map-responsive" align="center"  className="wcustomhtml">
                                 <div className="ab">
                                     <div className="abc">
                                         <iframe className="adc" scrolling="no" frameBorder="0" allowtransparency="true" width="50%" height="50%" src="//rf.revolvermaps.com/w/6/a/a2.php?i=5dv1qe7od2v&amp;m=7&amp;c=e63100&amp;cr1=ffffff&amp;f=arial&amp;l=0&amp;bv=90&amp;lx=-400&amp;ly=400&amp;hi=20&amp;he=7&amp;hc=a8ddff&amp;rs=80"></iframe>
